55c3ebbb6bbe19e8a5d0fe5ee19bb2db398df21e
Gabriel <gabrielbrodrigues1@gmail.com>
2025-03-16 17:58:56 -0300

    feat: add linkedList initial implementation with Nodes

---
+++ linked-lists/linkedList.js
@@ -0,0 +1,15 @@
+
+const Node = require('./node');
+
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1;
+    }
+}
+
+const myLinkedList = new LinkedList(10);
+
+console.log(myLinkedList);
